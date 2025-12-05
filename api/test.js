c4fbadbdd173a20e474226e690d4f38abda7d574
leeio6-lab <leeio6@naver.com>
2025-12-06 01:44:40 +0900

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({ 
+    message: 'API is working!',
+    timestamp: new Date().toISOString()
+  });
+};
